b6609d5e0e1ca20cf748be03467c261499ef4c5a
Chris Gilmer <chris.gilmer@ff0000.com>
2011-03-21 11:56:02 -0700

    Updated the set_hosts() method to allow for setting an individual machine name.

--- fabfile_example.py
+++ fabfile_example.py
@@ -29,18 +29,27 @@ def my_site():
 my_site()
 
 #--- Set all available hosts
-def set_hosts(stage='development',username='ubuntu'): 
+def set_hosts(stage='development',username='ubuntu',machine=''):
+	"""
+	The stage specifies which group of machines to set the hosts for
+
+	The username specifies which user to use when setting the hosts
+
+	The machine is optional and can specify a specific machine within
+	a stage to set the host for.
+	"""
     hosts = []
     PROVIDER = json.loads(open('fabric.conf','r').read()) 
     if stage in PROVIDER['machines']:
-        for name in PROVIDER['machines'][stage]: 
-            node_dict = PROVIDER['machines'][stage][name]
-			if 'public_ip' in node_dict:
-                public_ip = node_dict['public_ip']
-                for ip in public_ip:            
-                    hosts.append('%s@%s' % (username,ip))  
-            else:
-                fabric.api.warn(fabric.colors.yellow('No public IPs found for %s in %s' % (name,stage)))
+        for name in PROVIDER['machines'][stage]:
+			if (machine and machine == name) or not machine:
+            	node_dict = PROVIDER['machines'][stage][name]
+				if 'public_ip' in node_dict:
+            	    public_ip = node_dict['public_ip']
+            	    for ip in public_ip:            
+            	        hosts.append('%s@%s' % (username,ip))  
+            	else:
+            	    fabric.api.warn(fabric.colors.yellow('No public IPs found for %s in %s' % (name,stage)))
     env.hosts = hosts
     update_env()
 
