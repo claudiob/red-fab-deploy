589dff55ac0fd000b5da7970164464ad9a4ffd45
Chris Gilmer <chris.gilmer@ff0000.com>
2011-03-21 11:58:50 -0700

    Updated fabfile to use correct indentation.

--- fabfile_example.py
+++ fabfile_example.py
@@ -13,13 +13,13 @@ from fab_deploy import *
 def my_site():
 	""" Default Configuration """
 	env.conf = dict(
-        PROVIDER = 'ec2_us_east', # Also can use ec2_us_west
-        AWS_ACCESS_KEY_ID     = '',
-        AWS_SECRET_ACCESS_KEY = '',
-        
+		PROVIDER = 'ec2_us_east', # Also can use ec2_us_west
+		AWS_ACCESS_KEY_ID     = '',
+		AWS_SECRET_ACCESS_KEY = '',
+		
 		#PROVIDER = 'rackspace',
 		#RACKSPACE_USER = '',     
-        #RACKSPACE_KEY  = '',
+		#RACKSPACE_KEY  = '',
 		
 		CONF_FILE = os.path.join(os.getcwd(),'fabric.conf'),
 		INSTANCE_NAME = 'projectname', # Recommend no underscore characters
@@ -38,20 +38,20 @@ def set_hosts(stage='development',username='ubuntu',machine=''):
 	The machine is optional and can specify a specific machine within
 	a stage to set the host for.
 	"""
-    hosts = []
-    PROVIDER = json.loads(open('fabric.conf','r').read()) 
-    if stage in PROVIDER['machines']:
-        for name in PROVIDER['machines'][stage]:
+	hosts = []
+	PROVIDER = json.loads(open('fabric.conf','r').read()) 
+	if stage in PROVIDER['machines']:
+		for name in PROVIDER['machines'][stage]:
 			if (machine and machine == name) or not machine:
-            	node_dict = PROVIDER['machines'][stage][name]
+				node_dict = PROVIDER['machines'][stage][name]
 				if 'public_ip' in node_dict:
-            	    public_ip = node_dict['public_ip']
-            	    for ip in public_ip:            
-            	        hosts.append('%s@%s' % (username,ip))  
-            	else:
-            	    fabric.api.warn(fabric.colors.yellow('No public IPs found for %s in %s' % (name,stage)))
-    env.hosts = hosts
-    update_env()
+					public_ip = node_dict['public_ip']
+					for ip in public_ip:            
+						hosts.append('%s@%s' % (username,ip))  
+				else:
+					fabric.api.warn(fabric.colors.yellow('No public IPs found for %s in %s' % (name,stage)))
+	env.hosts = hosts
+	update_env()
 
 #--- Set up individual hosts
 def example():
